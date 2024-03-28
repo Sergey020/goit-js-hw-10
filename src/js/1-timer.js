e9018cb849b7b17768562b2079ef01d2413fad49
Sergey020 <sergeytkachenko020@gmail.com>
2024-03-28 22:18:18 +0200

    add lib

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,20 @@
+
+import flatpickr from "flatpickr"; 
+import "flatpickr/dist/flatpickr.min.css"
+
 console.log("timer");
+const imputEl = document.querySelector("#datetime-picker");
+// let userSelectedDate =
+const options = { 
+    enableTime: true, // вкл выбор времени
+    time_24hr: true, // Отображает выбор времени в 24-часовом режиме без выбора AM/PM, если этот параметр включен.
+    defaultDate: new Date(), //Устанавливает начальную выбранную дату(ы).
+    minuteIncrement: 1, //Начальное значение минутного элемента.
+    onClose(selectedDates) { 
+      console.log(selectedDates[0]); 
+    }, 
+  };
+
+function flatpickr(inputEl, options) {
+
+}
