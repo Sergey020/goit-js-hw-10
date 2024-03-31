9f016b21ffcbb627f72fced21fe2a9f2a0489c47
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-03-31 11:24:54 +0000

    Deploying to gh-pages from @ Sergey020/goit-js-hw-10@6a9dbe1197a061be6990b25432168b3e7b27d17d 🚀

--- assets/1-timer-3a12f85b.js
+++
@@ -1,2 +0,0 @@
-import{f as t}from"./vendor-2b44ac2e.js";console.log("timer");const o=document.querySelector("#datetime-picker"),n={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t(o,n);
-//# sourceMappingURL=1-timer-3a12f85b.js.map
